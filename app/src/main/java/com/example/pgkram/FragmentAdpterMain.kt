4f60a3aec9af7d2af237cec292615039a16c772a
Shagun <shagunchahar8@gmail.com>
2023-12-10 02:34:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pgkram/FragmentAdpterMain.kt
@@ -0,0 +1,28 @@
+package com.example.pgkram
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+
+class FragmentAdpterMain(fm: FragmentActivity):FragmentStateAdapter(fm){
+
+
+        override fun getItemCount(): Int {
+            return 4
+        }// Number of tabs
+
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                0 -> return Demographics()
+                1 -> return Demographics()
+                2 -> return Demographics()
+                3 -> return Demographics() // Add additional fragment for tab 4
+                else -> return Demographics()
+            }
+
+        }
+
+}
---
+++ app/src/main/java/com/example/pgkram/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.pgkram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+
+
+        val adapter = FragmentAdpterMain(this)
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager, TabLayoutMediator.TabConfigurationStrategy { tab, position ->
+            val tabNames = listOf("Demographics", "Dashboard", "Job success failure ratio", "Salary Insights")
+            tab.text = tabNames[position]
+        }).attach()
+
+
+    }
+}
